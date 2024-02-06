144aedaaaccb8548c1d2170fb5f0d949b0877490
Akshay Singh <singhakshay8794@gmail.com>
2024-02-06 21:51:32 +0530

    changes in cart

--- Motors/categ1_items/item1.js
+++ Motors/categ1_items/item1.js
@@ -1,83 +1,41 @@
-// showing all available seller 
-const showSeller = document.querySelector(".headBar");
-const check = document.getElementById("showList");
-showSeller.addEventListener('click', () => {
-    let hideList = document.getElementsByClassName("allSellerSection")[0];
-    if (!check.checked) {
-        check.checked = true
-        hideList.classList.add("hideList");
-        document.getElementsByClassName("content")[0].classList.add("squez")
-        document.getElementById("collapseIcon").style.transform = "rotate(180deg)"
-    }
-    else {
-        check.checked = false
-        hideList.classList.remove("hideList");
-        document.getElementsByClassName("content")[0].classList.remove("squez")
-        document.getElementById("collapseIcon").style.removeProperty("transform");
-    }
-})
-
-// selecting seller
-const isSellerSellected = document.getElementsByClassName("allSellerSection")[0].firstElementChild;
-const selectSeller = document.getElementsByClassName("allSellerSection")[0];
-const selectedSeller = (event) => {
-    let cnt = selectSeller.childElementCount;
-    for (let i = 2; i < cnt; i++) {
-        if (event.target.closest("." + selectSeller.children[i].className)) {
-            isSellerSellected.checked = true
-            return selectSeller.children[i].className;
+// check if element already present in cart
+const isSellerSellected = document.getElementsByClassName("sellerList")[0].firstElementChild;
+(function (){
+    console.log(basket);
+    basket.find((obj)=>{
+        let id = document.getElementsByClassName("itemImage")[0].firstElementChild.id;
+        if(obj.id == id){
+            isSellerSellected.checked = true; return;
         }
-    }
-}
-let sellerNo;
-selectSeller.addEventListener('click', (event) => { 
-    sellerNo = selectedSeller(event) 
-}) 
+    })
+})();
+
+const addItem = document.getElementById("cartButton");
+if(isSellerSellected.checked === true){
+    addItem.setAttribute("value", "Go to Cart");
+} 
 
 // adding current product in cart
-const addItem = document.getElementById("cartButton");
 const createCart = (event) => {
-    let changeContent = addItem.getAttribute("value");
-    if (isSellerSellected.checked === false && (changeContent !== "Go to Cart")) {
-        alert("Select Seller");
+    
+    if(isSellerSellected.checked === true){
+        location.href = "/Web Development/MiniProject/cart.html";
         return;
-    }
-    if (changeContent == "Go to Cart") {
-        location.href = "/Web Development/MiniProject/cart.html"
-    }
-    else {
-        let obj = 0;
-        let checkDup = document.getElementsByClassName("itemImage")[0].firstElementChild.id;
+    }       
+    isSellerSellected.checked = true;
+    basket.push({
+        id : document.getElementsByClassName("itemImage")[0].firstElementChild.id,
+        sellerId: document.getElementsByClassName("sellerList")[0].lastElementChild.id,
+        itemHref: document.documentURI,
         
-        while(obj < basket.length){
-            if (basket[obj].sellerId === sellerNo && basket[obj].id === checkDup){
-                return;
-            }
-            obj++;
-        }
-
-        basket.push({
-            id : document.getElementsByClassName("itemImage")[0].firstElementChild.id,
-            sellerId: sellerNo,
-            itemHref: document.documentURI,
-          
-            imageSrc: document.getElementsByClassName("itemImage")[0].firstElementChild.getAttribute("src"),
-            title: document.getElementsByClassName("title")[0].firstElementChild.innerText,
-            descriptContent: document.getElementsByClassName("descript")[0].firstElementChild.innerText,
-            sellerName: document.getElementsByClassName(sellerNo)[0].firstElementChild.children[0].innerText,
-            priceValue: document.getElementsByClassName(sellerNo)[0].firstElementChild.children[1].innerText,
-            sellerLoc: document.getElementsByClassName(sellerNo)[0].firstElementChild.children[2].innerText
-        })
-        localStorage.setItem("cartData", JSON.stringify(basket));
-        addItem.setAttribute("value", "Go to Cart");
-    }
-}
+        imageSrc: document.getElementsByClassName("itemImage")[0].firstElementChild.getAttribute("src"),
+        title: document.getElementsByClassName("title")[0].firstElementChild.innerText,
+        descriptContent: document.getElementsByClassName("descript")[0].firstElementChild.innerText,
+        sellerName: document.getElementById("motorSeller1Details").children[0].innerText,
+        priceValue: document.getElementById("motorSeller1Details").children[0].innerText,
+        sellerLoc: document.getElementById("motorSeller1Details").children[0].innerText
+    })    
+    localStorage.setItem("cartData", JSON.stringify(basket));
+}    
 addItem.addEventListener('click', createCart);
-
-// check if element already present in cart
-basket.find((obj)=>{
-    if(obj.id === document.getElementsByClassName("itemImage")[0].firstElementChild.id){
-        addItem.setAttribute("value", "Go to Cart");    return;
-    }
-})
 
